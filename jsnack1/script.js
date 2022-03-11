0ae193ba671c5730589ebf5a16df7100f0c64b0e
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-11 17:27:25 +0100

    primo esercizio

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,8 +1,11 @@
 //Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari.
 
+const numeri = [11, 45, 34, 77, 3, 6, 9];
+sommaNumeriDispari = 0;
 
-
-
-
+for(let i = 0; i < numeri.length; i++){
+  if(i % 2 !== 0);
+  sommaNumeriDispari += numeri[i];
+};
 
 console.log(sommaNumeriDispari);
