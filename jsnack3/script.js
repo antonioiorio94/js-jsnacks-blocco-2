9ae567a0cfbda0302c697b8da1d30df0118de2d8
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-13 17:04:53 +0100

    esercizio 3 finito

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,8 +1,15 @@
 //Crea due array che hanno un numero di elementi diversi. Aggiungi elementi casuali all’array che ha meno elementi, fino a quando ne avrà tanti quanti l’altro.
 
+
 const primoElenco = [1, 4, 8, 9, 5];
 const secondoElenco = [20, 2];
+randomNum = Math.floor(Math.random() * 10);
 
-while (primoElenco > secondoElenco){
-  secondoElenco.push(Math.random());
-}
+while (secondoElenco.length < primoElenco.length){
+  secondoElenco.push(randomNum);
+};
+
+
+
+console.log(primoElenco);
+console.log(secondoElenco);
